32307f3b48b4fd6fcd7dab2028f1b5cfbf8ac0c6
patman <27874300+patmanf@users.noreply.github.com>
2025-04-08 19:43:23 -0500

    crate breaks into debris . and it's loud!

---
+++ pat/terramartgun/projectiles/debris/debris.lua
@@ -0,0 +1,19 @@
+function init()
+  self.rotationSpeed = config.getParameter("rotationSpeed", 20)
+  mcontroller.setRotation(math.random() * math.pi * 2)
+
+  local ttlRange = config.getParameter("timeToLiveRange")
+  if ttlRange then
+    local ttl = ttlRange[1] + (math.random() * (ttlRange[2] - ttlRange[1]))
+    projectile.setTimeToLive(ttl)
+  end
+end
+
+function update(dt)
+  local vel = mcontroller.velocity()
+  local dir = vel[1] > 0 and 1 or -1
+  
+  local mag = math.sqrt(vel[1] * vel[1] + vel[2] * vel[2])
+  local rot = (mag / 180 * math.pi) * -dir * dt * self.rotationSpeed
+  mcontroller.setRotation(mcontroller.rotation() + rot)
+end
