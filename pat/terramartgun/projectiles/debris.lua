35da70aa260ed3eaf08922450405a5e6cc667267
patman <27874300+patmanf@users.noreply.github.com>
2025-08-01 19:37:33 -0500

    debris rotation can have  variance

--- pat/terramartgun/projectiles/debris.lua
+++ pat/terramartgun/projectiles/debris.lua
@@ -2,6 +2,11 @@ function init()
   self.rotationSpeed = config.getParameter("rotationSpeed", 20)
   mcontroller.setRotation(math.random() * math.pi * 2)
 
+  local variance = config.getParameter("rotationVariance")
+  if variance then
+    self.rotationSpeed = sb.nrand(variance, self.rotationSpeed)
+  end
+
   local ttlRange = config.getParameter("timeToLiveRange")
   if ttlRange then
     local ttl = ttlRange[1] + (math.random() * (ttlRange[2] - ttlRange[1]))
