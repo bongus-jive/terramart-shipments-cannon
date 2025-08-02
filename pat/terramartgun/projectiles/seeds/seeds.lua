32da7c6ee85f26c52aee5dc6e8a85bafcc31dace
patman <27874300+patmanf@users.noreply.github.com>
2025-08-01 20:10:29 -0500

    do several attempts at placing seeds

--- pat/terramartgun/projectiles/seeds/seeds.lua
+++ pat/terramartgun/projectiles/seeds/seeds.lua
@@ -1,12 +1,19 @@
 require "/pat/terramartgun/projectiles/debris.lua"
 
+local offsets = { {0, 0}, {-1, 0}, {1, 0} }
+
 function destroy()
   local seed = config.getParameter("seedType")
-  if seed then
-    local pos = entity.position()
-    local dir = math.random(0, 1) == 0 and -1 or 1
-    local params = config.getParameter("seedParameters")
+  if not seed then return end
+  
+  local pos = entity.position()
+  local dir = math.random(0, 1) == 0 and -1 or 1
+  local params = config.getParameter("seedParameters")
 
-    world.placeObject(seed, pos, dir, params)
+  for _, offset in pairs(offsets) do
+    local p = {pos[1] + offset[1], pos[2] + offset[2]}
+    if world.placeObject(seed, p, dir, params) then
+      break
+    end
   end
 end
